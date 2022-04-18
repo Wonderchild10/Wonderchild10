4154e096cd2bfd89e953288c345a8961e3ae7964
Wonderchild10 <102476289+Wonderchild10@users.noreply.github.com>
2022-04-18 17:04:34 +0800

    Add files via upload

---
+++ register_file.v
@@ -0,0 +1,17 @@
+module RegFile (clk, readreg1, readreg2, writereg, writedata, RegWrite, readdata1, readdata2);
+  input [4:0] readreg1, readreg2, writereg;
+  input [31:0] writedata;
+  input clk, RegWrite;
+  output [31:0] readdata1, readdata2;
+    
+  reg [31:0] regfile [0:31];
+
+  always @(posedge clk)	begin
+  	if (RegWrite) 
+	 	regfile[writereg] <= writedata;
+  end
+  
+  assign readdata1 = regfile[readreg1];
+  assign readdata2 = regfile[readreg2];
+
+endmodule
